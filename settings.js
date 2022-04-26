b9126d861a911f0d8ac92bf4cf519ae18cf3838b
Abegenk <102190428+Abegenk@users.noreply.github.com>
2022-04-26 21:54:12 +0700

    Initial commit

---
+++ settings.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	alfa: 'https://api-mlsidbot.herokuapp.com',
+}
+
+// Free apikey
+global.APIKeys = {
+	'https://api-mlsidbot.herokuapp.com': 'MlsidBot',
+}
+
+// setting 
+global.autoread = false // auto read pesan / message
+global.autorecording = true //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+
+// Other
+global.botname = "MlsidBotོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ A b e e yོ ×፝֟͜×" //nama kalian
+global.myweb ="https://api-MlsidBot.herokuapp.com/" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/AFKSQUAD" //bebas asal jan hapus
+global.github = "https://github.com/abegenk" //bebas
+global.email = "abilstoy@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.ownernomer = "6289510437264" // nomor wa kalian
+global.ownernomerr = "+6289510437264" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome="https://telegra.ph/file/Foto1.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.owner = ["6289510437264"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© MlsidBotོ' //sticker wm ubah
+global.author = 'Di Buat Oleh A b e e y' //sticker wm ganti nama kalian
+global.sessionName = 'session'
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.prefa = ['!','.','*','#']
+global.sp = '.'
+global.mess = {
+    sukses: '🤗Done, Oke Bree~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
